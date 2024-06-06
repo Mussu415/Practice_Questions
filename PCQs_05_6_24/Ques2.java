d625689002f947f6dcdcdf69a288f126af81e7bd
Mussu415 <muskankoku@gmail.com>
2024-06-06 10:48:27 +0530

    questions

--- PCQs_05_6_24/Ques2.java
+++ PCQs_05_6_24/Ques2.java
@@ -1,39 +1,61 @@
 package PCQs_05_6_24;
+import java.util.*;
+
+// roman to integer without addition
 
 public class Ques2 {
-       public static int romanToInt(String s) {
-            int result = 0;
+    public static void romanToInt(String s) {
+            int total = 0;
+            int current = 0;
             int previous = 0;
-            for (int i = s.length() - 1; i >= 0; i--) {
-                char c = s.charAt(i);
-                int current = 0;
-                
-                switch (c) {
-                    case 'I':
-                        current = 1;
-                        break;
-                    case 'V':
-                        current= 5;
-                        break;
-                    case 'X':
-                        current = 10;
-                        break;
-                }
-                if (current < previous) {
-                    result -= current;
-                } else { 
-                    result += current;
-                }
-                
-                previous = current;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i+1);
+            char p = s.charAt(i);
+            
+          
+            switch (c) {
+                case 'I':
+                    current = 1;
+                    break;
+                case 'V':
+                    current = 5;
+                    break;
+                case 'X':
+                    current = 10;
+                    break;
             }
             
-            return result;
+            switch (p) {
+                case 'I':
+                    previous = 1;
+                    break;
+                case 'V':
+                    previous = 5;
+                    break;
+                case 'X':
+                    previous = 10;
+                    break;
+            }
+            
+            if(current == previous){
+             System.out.print(previous);
+            }
+            else if (previous > current){
+              System.out.print(previous);
+            }
+            else{
+              System.out.println(current-previous);
+              i++;
+            }
         }
         
-        public static void main(String[] args) {
-            String romanNumber = "XXIX";
-            System.out.println("Output:- " + romanToInt(romanNumber));
-        }
+        // return total;
     }
 
+    public static void main(String[] args) {
+        String romanNumber = "XXIV";
+        System.out.println("The output is:- ");
+        romanToInt(romanNumber);
+    }
+}
---
+++ PCQs_05_6_24/Ques9.java
@@ -0,0 +1,22 @@
+package PCQs_05_6_24;
+public class Ques9 {
+    public static long nCr(int n, int r) {
+            // Base cases
+            if (r == 0 || n == r) {
+                return 1;
+            } else if (r < 0 || n < 0) {
+                return 0; // Handle invalid inputs (negative r or n)
+            } else {
+                // Recursive case: nCr = nC(n-1, r) + nC(n-1, r-1)
+                return nCr(n - 1, r) + nCr(n - 1, r - 1);
+            }
+        }
+        public static void main(String[] args) {
+            int n = 5;
+            int r = 2;
+            long result = nCr(n, r);
+            System.out.println("The value of " + n + "C" + r + " is " + result);
+        }
+    }
+    
+
